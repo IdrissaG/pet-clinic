e1fd5724508f9ce11abc8a8148a4dc5ea25e3435
Lazare DJOBO <lazaredjobo01@gmail.com>
2026-08-20 15:00:23 +0000

    Ajout module Cliniques : entités, services, vues et traductions

--- src/main/java/com/stg/petclinic/repository/MedecinRepository.java
+++ src/main/java/com/stg/petclinic/repository/MedecinRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface MedecinRepository extends JpaRepository<Medecin, Long> {}
+public interface MedecinRepository extends JpaRepository<Medecin, Long> {
+    boolean existsByCliniqueId(Long cliniqueId);
+}
